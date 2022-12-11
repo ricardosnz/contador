fa6afac368d79637f11e54c3d804eb01905634ca
RicardoSan12 <73980150+RicardoSan12@users.noreply.github.com>
2022-12-11 02:46:00 -0600

    control

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './style.css';
 import Svg from '../Svg';
 import { setting } from '../../utils';
- 
-const typesClass = { settings: 'pomodoro-app__preferences', close: 'pane__close-preferences', apply: 'pane__apply-preferences' };
 
-const Button = ({ type = '', buttonText = '', toggleVisibility }) => Object.keys(typesClass).includes(type) && <div className={'pane__apply-row'}>
-    <button
-    className={typesClass[type]}
-    name={type == 'settings' && 'settings'}
-    onClick={toggleVisibility}
-    >
-    {type !== 'settings' ? buttonText : <Svg pathD={setting} />}
-  </button>
-</div>
+const typesClass = {
+  settings: 'pomodoro-app__preferences',
+  close: 'pane__close-preferences',
+  apply: 'pane__apply-preferences',
+};
+
+const Button = ({ type = '', buttonText = '', toggleVisibility }) =>
+  Object.keys(typesClass).includes(type) && (
+    <button className={typesClass[type]} name={type == 'settings' && 'settings'} onClick={toggleVisibility}>
+      {type !== 'settings' ? buttonText : <Svg pathD={setting} />}
+    </button>
+);
 
 export default Button;
